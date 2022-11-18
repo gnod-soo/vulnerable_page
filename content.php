baadde10da8b55b852864e8aabcc809193526cb1
한동수 <ehdtn3385@gmail.com>
2022-11-18 00:31:38 +0000

    content page

---
+++ content.php
@@ -0,0 +1,23 @@
+<?php
+	$num = $_GET['num'];
+	$id = $_GET['id'];
+	$title = $_GET['title'];
+	$description = $_GET['description'];
+	
+	echo '<h3>title: '.$title.'</h3>';
+	echo '<h4>writer: '.$id.'</h4>';
+            echo $description.'</li>';
+            echo '
+            <form action="delete_process.php" method="post">
+                <input type="hidden" name="title" value="'.$title.'">
+                <input type="hidden" name="id" value="'.$id.'">
+                <input type="submit" value="삭제">
+            </form>
+            <form action="update.php" method="post">
+                <input type="hidden" name="title" value="'.$title.'">
+                <input type="hidden" name="description" value="'.$description.'">
+                <input type="hidden" name="id" value="'.$id.'">
+                <input type="submit" value="수정">
+            </form>
+            ';
+?>
--- read_process.php
+++ read_process.php
@@ -11,22 +11,8 @@
         ";
         $result = mysqli_query($conn, $sql);
         while($row=mysqli_fetch_array($result)){
-            echo '<li><h2>'.$row['title'].'</h2>';
-			echo '<h4>writer: '.$row['id'].'</h4>';
-            echo $row['description'].'</li>';
-            echo '
-            <form action="delete_process.php" method="post">
-                <input type="hidden" name="title" value="'.$row['title'].'">
-                <input type="hidden" name="id" value="'.$row['id'].'">
-                <input type="submit" value="삭제">
-            </form>
-            <form action="update.php" method="post">
-                <input type="hidden" name="title" value="'.$row['title'].'">
-                <input type="hidden" name="description" value="'.$row['description'].'">
-                <input type="hidden" name="id" value="'.$row['id'].'">
-                <input type="submit" value="수정">
-            </form>
-            ';
+            echo '<li><h2><a href="content.php?num='.$row['num'].'&title='.$row['title'].'&description='.$row['description'].'&id='.$row['id'].'">'.$row['title'].'</a></h2>';
+			
         }
     }
 ?>
